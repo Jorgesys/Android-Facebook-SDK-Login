a0e948f7a9594e318a9fc4e8dde8ffa63e2d2c31
Jorgesys <jorge.osorio@gmail.com>
2021-10-13 12:43:59 -0500

    Update MainActivity.java

--- app/src/main/java/com/jorgesys/loginfacebook/MainActivity.java
+++ app/src/main/java/com/jorgesys/loginfacebook/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -14,6 +16,7 @@ import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import com.facebook.LoginStatusCallback;
+import com.facebook.Profile;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
@@ -22,12 +25,15 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.net.MalformedURLException;
+import java.util.Arrays;
 
 /*
+jorgesys12@gmail.com
+https://stackoverflow.com/users/250260/jorgesys
+
 Login for android using Android SDK.
 https://developers.facebook.com/docs/facebook-login/android
 app settings
-https://developers.facebook.com/apps/331877241457/settings/basic/
 https://developers.facebook.com/apps/
  */
 
@@ -42,11 +48,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        /*---------------Facebook retrieveLoginStatus -------------------*/
         //10. Comprobar el estado del inicio de sesión
         AccessToken accessToken = AccessToken.getCurrentAccessToken();
         boolean isLoggedIn = accessToken != null && !accessToken.isExpired();
         Log.i( TAG, "onCreate() isLoggedIn: " + isLoggedIn);
-
         //11. Activar el inicio de sesión rápido
         LoginManager.getInstance().retrieveLoginStatus(this, new LoginStatusCallback() {
             @Override
@@ -54,9 +60,9 @@ public class MainActivity extends AppCompatActivity {
                 // User was previously logged in, can log them in directly here.
                 // If this callback is called, a popup notification appears that says
                 // "Logged in as <User Name>"
-                Log.i( TAG, "onCompleted() getApplicationId" + accessToken.getApplicationId());
-                Log.i( TAG, "onCompleted() getUserId" + accessToken.getUserId());
-                Log.i( TAG, "onCompleted() getToken" + accessToken.getToken());
+                Log.i( TAG, "retrieveLoginStatus() onCompleted() getApplicationId: " + accessToken.getApplicationId());
+                Log.i( TAG, "retrieveLoginStatus() onCompleted() getUserId: " + accessToken.getUserId());
+                Log.i( TAG, "retrieveLoginStatus() onCompleted() getToken: " + accessToken.getToken());
             }
             @Override
             public void onFailure() {
@@ -67,20 +73,21 @@ public class MainActivity extends AppCompatActivity {
                 Log.e( TAG, "onError() exception " + exception.getMessage());
             }
         });
-
-
+        /*--------------------------------------------------------------------*/
 
         //9. Registrar una devolución de llamada
         callbackManager = CallbackManager.Factory.create();
-        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+
+      /*---------------Authentication using LoginButton -------------------*/
+        /*LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
         // Callback registration
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                Log.i( TAG, "LoginButton onSuccess() loginResult" + loginResult.hashCode());
-                Log.i( TAG, "LoginButton onSuccess() getUserId" + loginResult.getAccessToken().getUserId());
-                Log.i( TAG, "LoginButton onSuccess() getApplicationId" + loginResult.getAccessToken().getApplicationId());
-                Log.i( TAG, "LoginButton onSuccess() getToken" + loginResult.getAccessToken().getToken());
+                Log.i( TAG, "LoginButton onSuccess() loginResult: " + loginResult.hashCode());
+                Log.i( TAG, "LoginButton onSuccess() getUserId: " + loginResult.getAccessToken().getUserId());
+                Log.i( TAG, "LoginButton onSuccess() getApplicationId: " + loginResult.getAccessToken().getApplicationId());
+                Log.i( TAG, "LoginButton onSuccess() getToken: " + loginResult.getAccessToken().getToken());
             }
 
             @Override
@@ -92,7 +99,43 @@ public class MainActivity extends AppCompatActivity {
             public void onError(FacebookException exception) {
                 Log.e(TAG, "LoginButton onError() " + exception.getMessage());
             }
+        });*/
+        /*---------------------------------------------------------------*/
+
+        /*---------------Call automatic authentication in onCreate()-------------------------*/
+        LoginManager manager = LoginManager.getInstance();
+        manager.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Profile.fetchProfileForCurrentAccessToken();
+                Log.i(TAG, "manager.registerCallback onSuccess() getUserId: " + loginResult.getAccessToken().getUserId());
+                Log.i(TAG, "manager.registerCallback onSuccess() getApplicationId: " + loginResult.getAccessToken().getApplicationId());
+                Log.i(TAG, "manager.registerCallback onSuccess() getToken: " + loginResult.getAccessToken().getToken());
+            }
+            @Override
+            public void onError(FacebookException exception) {
+                AccessToken.setCurrentAccessToken(null);
+                //currentUserChanged();
+                Log.e(TAG, "manager.registerCallback onError()! "  + exception.getMessage());
+            }
+            @Override
+            public void onCancel() {
+                AccessToken.setCurrentAccessToken(null);
+                Log.e(TAG, "manager.registerCallback onCancel()! ");
+            }
         });
+
+        manager.logIn(this, Arrays.asList("public_profile"/*, "user_friends" : invalid scope*/) );
+
+        Button buttonLogOut = (Button) findViewById(R.id.btnLogOut);
+        buttonLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Log OUT
+                LoginManager.getInstance().logOut();
+            }
+        });
+        /*---------------------------------------*/
 
 
     }
@@ -103,7 +146,6 @@ public class MainActivity extends AppCompatActivity {
         Log.e(TAG, "onActivityResult() requestCode " + requestCode + " resultCode " + resultCode);
         super.onActivityResult(requestCode, resultCode, data);
     }
-
 
 }
 
